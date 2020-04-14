e8593da876a6b1dfd470f6f58553c2d6ba136688
Timur <teemychteemych@gmail.com>
2020-04-14 17:27:19 +0300

    Added one test for new syntax

--- lib/test/check_pattern.c
+++ lib/test/check_pattern.c
@@ -1105,6 +1105,88 @@ START_TEST (parse_all_shortest_paths)
 }
 END_TEST
 
+START_TEST (parse_path_pattern)
+{
+    struct cypher_input_positionlast = cypher_input_position_zero;
+    result = cypher_parse("MATCH (me)-/~cousin/-(my_cousin);",
+            &last, NULL, 0);
+    ck_assert_ptr_ne(result, NULL);
+    ck_assert_int_eq(last.offset, 32);
+    
+    ck_assert(cypher_parse_result_fprint_ast(result, memstream, 0, NULL, 0) == 0);
+    fflush(memstream);
+    const char *expected = "\n"
+" @0   0..33  statement                                 body=@1\n"
+" @1   0..33  > query                                   clauses=[@2]\n"
+" @2   0..33  > > MATCH                                 pattern=@3\n"
+" @3   6..32  > > > pattern                             paths=[@4]\n"
+" @4   6..32  > > > > pattern path                      (@5)-/@7/-(@13)\n"
+" @5   6..10  > > > > > node pattern                    (@6)\n"
+" @6   7..9   > > > > > > identifier                    `me`\n"
+" @7  10..21  > > > > > path pattern                    -/@8/-\n"
+" @8  12..19  > > > > > > path expression               @9\n"
+" @9  12..19  > > > > > > > alternative                 @10\n"
+"@10  12..19  > > > > > > > > path base                 @11\n"
+"@11  12..19  > > > > > > > > > path pattern reference  ~@12\n"
+"@12  13..19  > > > > > > > > > > identifier            `cousin`\n"
+"@13  21..32  > > > > > node pattern                    (@14)\n"
+"@14  22..31  > > > > > > identifier                    `my_cousin`\n";
+    ck_assert_str_eq(memstream_buffer, expected);
+    
+    const cypher_astnode_t *ast = cypher_parse_result_get_directive(result, 0);
+    const cypher_astnode_t *query = cypher_ast_statement_get_body(ast);
+    const cypher_astnode_t *match = cypher_ast_query_get_clause(query, 0);
+    
+    const cypher_astnode_t *pattern = cypher_ast_match_get_pattern(match);
+    ck_assert_int_eq(cypher_astnode_type(pattern), CYPHER_AST_PATTERN);
+    
+    ck_assert_int_eq(cypher_ast_pattern_npaths(pattern), 1);
+    const cypher_astnode_t *path = cypher_ast_pattern_get_path(pattern, 0);
+    ck_assert_int_eq(cypher_astnode_type(path), CYPHER_AST_PATTERN_PATH);
+    
+    ck_assert_int_eq(cypher_ast_pattern_path_nelements(path), 3);
+    const cypher_astnode_t *node = cypher_ast_pattern_path_get_element(path, 0);
+    ck_assert_int_eq(cypher_astnode_type(node), CYPHER_AST_NODE_PATTERN);
+
+    const cypher_astnode_t *id = cypher_ast_node_pattern_get_identifier(node);
+    ck_assert_int_eq(cypher_astnode_type(id), CYPHER_AST_IDENTIFIER);
+    ck_assert_str_eq(cypher_ast_identifier_get_name(id), "me");
+    
+    ck_assert_int_eq(cypher_ast_node_pattern_nlabels(node), 0);
+    ck_assert_ptr_eq(cypher_ast_node_pattern_get_label(node, 0), NULL);
+    ck_assert_ptr_eq(cypher_ast_node_pattern_get_properties(node), NULL);
+    
+    const cypher_astnode_t *path = cypher_ast_pattern_path_get_element(path, 1);
+    ck_assert_int_eq(cypher_astnode_type(path), CYPHER_AST_PATH_PATTERN);
+    
+    ck_assert_int_eq(cypher_ast_path_pattern_get_direction(path),
+            CYPHER_REL_BIDIRECTIONAL);
+    
+    const cypher_astnode_t *expr = cypher_path_pattern_get_expression(path);
+    ck_assert_int_eq(cypher_astnode_type(expr), CYPHER_AST_PATH_PATTERN_EXPRESSION);
+    ck_assert_int_eq(cypher_ast_path_pattern_expression_get_nelements(expr), 1);
+    ck_assert_ptr_eq(cypher_ast_path_pattern_expression_get_element(expr, 1), NULL);
+    
+    const cypher_astnode_t *alt = cypher_path_pattern_expression_get_element(expr, 0);
+    ck_assert_int_eq(cypher_astnode_type(alt), CYPHER_AST_PATH_PATTERN_ALTERNATIVE);
+    ck_assert_int_eq(cypher_ast_path_pattern_alternative_get_nelements(alt), 1);
+    ck_assert_int_eq(cypher_ast_path_pattern_alternative_get_element(alt, 1), NULL);
+    
+    const cypher_astnode_t *base = cypher_path_pattern_alternative_get_element(alt, 0);
+    ck_assert_int_eq(cypher_astnode_type(base), CYPHER_AST_PATH_PATTERN_BASE);
+    ck_assert_int_eq(cypher_ast_path_pattern_base_get_direction(base), CYPHER_REL_BIDIRECTIONAL);
+    
+    const cypher_astnode_t *ref = cypher_ast_path_pattern_base_get_child(base);
+    ck_assert_str_eq(cypher_ast_path_pattern_reference_get_identifier(ref), "cousin");
+    
+    const cypher_astnode_t *node2 = cypher_ast_pattern_path_get_element(path, 2);
+    ck_assert_int_eq(cypher_astnode_type(node2), CYPHER_AST_NODE_PATTERN);
+
+    const cypher_astnode_t *id2 = cypher_ast_node_pattern_get_identifier(node2);
+    ck_assert_int_eq(cypher_astnode_type(id2), CYPHER_AST_IDENTIFIER);
+    ck_assert_str_eq(cypher_ast_identifier_get_name(id2), "my_cousin");
+}
+END_TEST
 
 TCase* pattern_tcase(void)
 {
@@ -1125,5 +1207,6 @@ TCase* pattern_tcase(void)
     tcase_add_test(tc, parse_named_path);
     tcase_add_test(tc, parse_shortest_path);
     tcase_add_test(tc, parse_all_shortest_paths);
+    tcase_add_test(tc, parse_path_pattern);
     return tc;
 }
